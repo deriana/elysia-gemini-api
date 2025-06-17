91d3c6160412f8fc7857603b8b58765cdaaa02f8
deriana <derana765@gmail.com>
2025-06-17 13:33:56 +0700

    fix structure and delete gak penting

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,12 @@
 import { Elysia } from "elysia";
 import { geminiRoute } from "./routes/gemini.route";
 import dotenv from "dotenv";
-import { animeRoute } from "./routes/anime.route";
-import { dokterRoute } from "./routes/dokter.route";
-import { loveLiveRoute } from "./routes/lovelive.route";
 
 dotenv.config();
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .use(geminiRoute)
-  .use(animeRoute)
-  .use(dokterRoute)
-  .use(loveLiveRoute)
   .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
